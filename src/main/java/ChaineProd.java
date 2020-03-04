4b77f3390d3c3b5c2e6695f74fac34da3c182021
Axel <axelly.ly@gmail.com>
2020-03-04 18:45:38 +0100

    Ajout de quelques méthodes

--- src/main/java/ChaineProd.java
+++ src/main/java/ChaineProd.java
@@ -1,9 +1,43 @@
 import java.util.HashMap;
 
 public class ChaineProd{
+    /// Les attributs
     private String codeC;
     private String nomC;
     private int niveauA;
     private HashMap<Element, Integer> entrees;
     private HashMap<Element, Integer> sorties;
+
+    /// LES GETTEURS
+    public String getCodeC() {
+        return codeC;
+    }
+
+    public String getNomC() {
+        return nomC;
+    }
+
+    public int getNiveauA() {
+        return niveauA;
+    }
+
+    public HashMap<Element, Integer> getEntrees() {
+        return entrees;
+    }
+
+    public HashMap<Element, Integer> getSorties() {
+        return sorties;
+    }
+
+    /// Constructeur
+
+    public ChaineProd(String codeC, String nomC, int niveauA, HashMap<Element, Integer> entrees, HashMap<Element, Integer> sorties) {
+        this.codeC = codeC;
+        this.nomC = nomC;
+        this.niveauA = niveauA;
+        this.entrees = entrees;
+        this.sorties = sorties;
+    }
+
+    /// LES METHODES
 }
--- src/main/java/Usine.java
+++ src/main/java/Usine.java
@@ -1,11 +1,94 @@
 import java.util.ArrayList;
 
-public class Usine {
-    private ArrayList<Element> lesProduits;
-    private ArrayList<Element> lesMP;
-    private ArrayList<ChaineProd> lesChaines;
+public class Usine{
 
+    // LES ATTRIBUTS
+    private ArrayList<Element> leStock;
+    private ArrayList<ChaineProd> lesChaines;
+    private ArrayList<Element> lesAchats;
+
+    /// LES GETTEURS
+    public ArrayList<Element> getLeStock() {
+        return leStock;
+    }
+    public ArrayList<ChaineProd> getLesChaines() {
+        return lesChaines;
+    }
+
+    public HashMap<Element, Integer> getLesAchats() {
+        return lesAchats;
+    }
+    /// CONSTRUCTEUR
     public Usine(){
-        
+        leStock = new ArrayList<Element>();
+        lesChaines = new ArrayList<ChaineProd>();
+        lesAchats = new HashMap<Element, Integer>();
+    }
+
+
+    /// LES METHODES
+
+    /**
+     * Ajoute l'élément/prod dans la liste de stock
+     * @param leprod = le produit à ajouter au stock
+     */
+    public void stocker(Element leprod){
+        leStock.add(leprod);
+    }
+
+    /**
+     * Supprime l'élément du stock
+     * @param elem = l'élément à supprimer
+     */
+    public void destocker(Element elem){
+        // Faire le test de savoir si l'élément est dans la liste du stock ou non
+        int i = 0;
+        while(leStock[i]!=elem)&&(i<leStock.size()){
+            i++;
+        }
+        leStock.remove(i);
+    }
+
+    /**
+     * Ajoute un élément dans la liste d'achat
+     * @param elem = l'élément à ajouter
+     */
+    public void ajouterunAchat(Element elem){
+        lesAchats.add(elem);
+    }
+
+    /**
+     * Permet de supprimer un élément de la liste d'achat
+     * @param elem = l'élément/produit à supprimer de la liste
+     */
+    public void supprimerunAchat(Element elem){
+        // Il faut vérifier que l'élément soit dans la liste
+        int i = 0;
+        while(lesAchats[i]!=elem)&&(i<leStock.size()){
+            i++;
+        }
+        leStock.remove(i);
+    }
+
+    /**
+     * Permet de calculer le coût total de la liste d'achat
+     * @return le coût total
+     */
+    public int lecoutdesAchat(){
+        int somme = 0;
+        for (int i = 0; i<lesAchats.size();i++){
+            somme += lesAchats[i].prixA;
+        }
+        return somme;
+    }
+
+    /**
+     * Permet d'acheter tout les éléments présents dans la liste d'achat
+     */
+    public void acheter(){
+        for (int i = 0; i<lesAchats.size();i++){
+            stocker(lesAchats[i]);
+        }
+        //Affichage de la somme à payer
     }
 }
