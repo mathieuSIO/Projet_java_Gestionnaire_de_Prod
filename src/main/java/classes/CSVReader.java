7cdbf2ad4d01ffba5cbe58326fbb150595609e01
Axel-Ly <axelly.ly@gmail.com>
2020-03-19 13:53:22 +0100

    Create CSVReader.java

---
+++ src/main/java/classes/CSVReader.java
@@ -0,0 +1,25 @@
+package classes;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class CSVReader {
+
+	public static void main(String[] args) throws IOException{
+		File csvFile = new File("C:/Users/Ly/Documents/L3MIASHS/S6/csvprojet/elements.csv");
+			BufferedReader br = new BufferedReader(new FileReader(csvFile));
+			String line = "";
+			try {
+				while((line=br.readLine())!=null ) {
+					String[] count = line.split(",");
+					System.out.println(count[0]);
+				}
+			}	catch(FileNotFoundException e) {
+				e.printStackTrace();
+			}
+	}
+}
