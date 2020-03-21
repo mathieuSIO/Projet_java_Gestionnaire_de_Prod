290d03708953bb6be874cb4bbbbf6c1a8bbd148a
sese-la <valentin.lacroix94@hotmail.fr>
2020-03-21 14:16:24 +0100

    main
    main à  modifier

--- src/main/java/controller/MainApp.java
+++ src/main/java/controller/MainApp.java
@@ -1,5 +1,56 @@
 package controller;
 
-public class MainApp {
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+
+	public MainApp() {
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("GestionProd");
+
+		initRootLayout();
+
+	//	showPersonOverview();
+	}
+	
+	public void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/ChaineDeProd.fxml"));
+			rootLayout = (BorderPane) loader.load();
+
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
